9fcbd9f4b1abff0f7f17ecc293457b6244f7d28e
GaganTut <gstut@live.com>
2017-07-18 01:24:01 -0700

    Added event listeners to links to navigate to different views

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('helooooo');
+/*jshint esversion: 6*/
+console.log('hiiiii');
+
+(global => {
+  const views = document.querySelectorAll('.views');
+  const mainLinks = document.querySelectorAll('.main-links');
+
+  mainLinks.forEach(link => {
+    console.log(link);
+    link.addEventListener('click', event => {
+      console.log(event.target);
+    });
+  });
+})(window);
