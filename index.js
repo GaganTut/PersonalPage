be5dd7bfe6df2d599ee11b584e9194dbba6af12a
GaganTut <gstut@live.com>
2017-07-18 19:40:47 -0700

    Added views with links to change the different views

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 /*jshint esversion: 6*/
-console.log('hiiiii');
-
 (global => {
   const views = document.querySelectorAll('.views');
   const mainLinks = document.querySelectorAll('.main-links');
+  views[0].style.zIndex = '1';
+  const linkViewPositions = {
+    home: 0,
+    projects: 1,
+    resume: 2,
+    contact: 3
+  };
 
   mainLinks.forEach(link => {
-    console.log(link);
     link.addEventListener('click', event => {
-      console.log(event.target);
+      views.forEach(view => view.style.zIndex = '0');
+      views[linkViewPositions[event.target.attributes.linktarget.value]].style.zIndex = '1';
     });
   });
 })(window);
