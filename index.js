30c565cc71df39211cc48ab90bfa17ddbc8649ba
GaganTut <gstut@live.com>
2017-08-11 12:40:06 -0700

    Styling up navigation and resume page for easier reading and more space

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 /*jshint esversion: 6*/
 (global => {
   const views = document.querySelectorAll('.views');
+  const navigation = document.querySelector('.navigation');
   const mainLinks = document.querySelectorAll('.main-links');
   views.forEach(view => view.style.display = 'none');
   views[0].style.display = 'block';
@@ -11,10 +12,21 @@
     contact: 3
   };
 
+  const navigationBG = {
+    home: false,
+    projects: true,
+    resume: true,
+    contact: true
+  };
+
   mainLinks.forEach(link => {
     link.addEventListener('click', event => {
       views.forEach(view => view.style.display = 'none');
       views[linkViewPositions[event.target.attributes.linktarget.value]].style.display = 'block';
+
+      navigationBG[event.target.attributes.linktarget.value] ?
+        (navigation.style.backgroundColor = 'rgb(240,240,240)') :
+        (navigation.style.backgroundColor = 'transparent');
     });
   });
 })(window);
